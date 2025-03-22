57e7fc5fa2d874b88fce468eec64890818e77935
Kelci <k.mohrman@ufl.edu>
2025-03-22 16:09:47 -0400

    Add ci

---
+++ tests/test_skimmer_local.py
@@ -0,0 +1,17 @@
+import subprocess
+from os.path import exists
+
+def test_ewkcoffea():
+    args = [
+        "time",
+        "python",
+        "analysis/template_4l/run_skimmer.py",
+        "input_samples/sample_jsons/test_samples/for_ci.json",
+        "-x",
+        "local",
+    ]
+
+    # Run ewkcoffea
+    subprocess.run(args)
+
+    assert (exists('skimtest/for_ci/skimmed-part0.root'))
