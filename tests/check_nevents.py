094e067a9aec0b5de9b87059b6edfcf3bcb15b2b
Kelci <k.mohrman@ufl.edu>
2025-09-04 18:17:33 -0400

    Forgot some names

--- tests/check_nevents.py
+++ tests/check_nevents.py
@@ -2,7 +2,7 @@ from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
 
 def test_nevents():
 
-    fname = "analysis/template_4l/skimtest/for_ci/skimmed-part0.root"
+    fname = "analysis/template_4l/skimtest/for_ci_0.root"
 
     events = NanoEventsFactory.from_root(
         {fname: "Events"},
